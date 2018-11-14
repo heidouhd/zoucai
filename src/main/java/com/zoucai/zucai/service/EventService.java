04d0f0d01024bd5509c6ab3c94fc41924fc56438
qingmengyixing <liup0725@163.com>
2018-11-14 19:08:24 +0800

    tijiao

--- src/main/java/com/zoucai/zucai/service/EventService.java
+++ src/main/java/com/zoucai/zucai/service/EventService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service(value = "eventService")
 public class EventService {
     @Autowired
-    private EventMapper eventMapper;//这里会报错，但是并不会影响
+    private EventMapper  eventMapper;//这里会报错，但是并不会影响
 
     public void insertData(Event event){
         eventMapper.insert(event);
